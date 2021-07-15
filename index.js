ef8c8aee4c5c45264b16ef875dbfd302f6005a20
Russian hacker boi <77713906+hackerboi72@users.noreply.github.com>
2021-07-15 16:45:09 +0530

    sorry if the html has nothing

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("hello javascript")
+var shourya = "shourya"
+var jain = "jain"
+var shouryajain = shourya + jain
+console.log(shouryajain)
+
+var express = shourya.concat(jain)
+console.log(express)
+console.log("shouryajain has 11 letters")
+var name = ("John Smith")
+console.log(name.replace("John", "MClaren"))
+console.log(name.toLowerCase())
+console.log(Math.sqrt(420))
+console.log(Math.max(69, 420))
+console.log(Math.pow(16, 19))
